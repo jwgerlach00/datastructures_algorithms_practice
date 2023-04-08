a517818f5c73ce8627500d04ed2b0d489d2bc3a7
Jacob Gerlach <jwgerlach00@gmail.com>
2023-04-07 21:17:24 -0400

    recursion

---
+++ recursion.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def count_up(max_num:int) -> List[int]:
+    '''Return a list of numbers from 1 to max_num, inclusive.'''
+    if max_num == 1: # base case --> simplist case, stops from looping forever
+        return [1]
+    return count_up(max_num - 1) + [max_num]
+
+def factorial(n:int) -> int:
+    '''n*...*4*3*2*1'''
+    if n == 1: # base case --> simplist case, stops from looping forever
+        return 1
+    return n * factorial(n-1)
+
+
+if __name__ == '__main__':
+    assert count_up(10) == list(range(1, 10+1))
+    assert factorial(5) == 5*4*3*2*1
